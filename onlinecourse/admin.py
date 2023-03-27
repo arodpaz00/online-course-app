aa2ee4e7b37ac8a2abc420e09056edf91d63b555
Andres <127614577+arodpaz00@users.noreply.github.com>
2023-03-27 23:40:56 +0200

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -23,7 +23,23 @@ class LessonAdmin(admin.ModelAdmin):
 
 
 # <HINT> Register Question and Choice models here
+class Question(admin.ModelAdmin):
+    course = admin.ForeignKey(Course, on_delete=models.CASCADE, related_name='questions')
+    text = admin.CharField(max_length=500)
+    grade_point = admin.DecimalField(max_digits=5, decimal_places=2, default=0.0)
 
+    def __str__(self):
+        return f'{self.course.name} - Question {self.id}: {self.text}' # pylint: disable=no-member
+
+    
+# class Choice(models.Model):
+class Choice(admin.ModelAdmin):
+    question = admin.ForeignKey(Question, on_delete=models.CASCADE)
+    text = admin.CharField(max_length=500)
+    is_correct = admin.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
